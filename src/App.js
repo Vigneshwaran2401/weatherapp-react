2f844a13b29bcad265be178a9a98601eda7636cf
Vigneshwaran2401 <vicky09989@gmail.com>
2024-02-02 13:09:58 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    //ee949cc184264765ad2f53c854eb1a81
+    const [degree, setDegree] = useState(0);
+    const [city, setCity] = useState("France");
+    const [description, setDescription] = useState("Raining");
+    const [input, setInput] = useState("");
+    const [high, setHigh] = useState(0);
+    const [low, setLow] = useState(0);
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const getData = () => {
+        const key = "05eb9f4cdcc2cd8c41198a5d8c0d306e";
+        const api = axios(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${key}`);
+
+        api.then((dalta)=> {
+            setDegree(dalta.data.main.temp);
+            setCity(dalta.data.name);
+            setDescription(dalta.data.weather[0].main);
+            setHigh(dalta.data.main.temp_max);
+            setLow(dalta.data.main.temp_min);
+        })
+    }
+    return( 
+    <div className="flex flex-row justify-center h-[100vh] items-center">
+        <div className="p-4 rounded-md shadow flex flex-col" style={{backgroundImage:"linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)"}} >
+        <h2 className="font-bold">Hello!⛅</h2>
+        <p className="text-s">Do you want to know the weather report :)</p>
+        <input type="text" className="rounded-md h-6 text-sm mt-2 p-1 outline-none" placeholder="City Name?" onChange={handleChange} />
+        <button onClick={getData} className="bg-black text-white rounded-lg p-1 mt-2 text-xs">Get Report!⚡</button>
+        <p className="text-s mt-2">Degree: {degree}°C | City: {city} | Weather : {description}</p>
+        <p className="text-s mt-2">High/Low: {high}°C / {low} °C</p>
+        </div>
+    </div>);
 }
 
 export default App;
